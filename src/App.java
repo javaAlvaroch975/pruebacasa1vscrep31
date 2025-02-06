dd7e55046f25b41d79d2bbb24003eaf837822eac
alvaroch975 <alvaroch975@gmail.com>
2025-02-06 08:37:52 +0100

    Update App.java

--- src/App.java
+++ src/App.java
@@ -3,5 +3,8 @@ public class App {
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+
     }
 }
